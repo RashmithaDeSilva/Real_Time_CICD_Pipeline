b09e8555b3849b15f4532c506ea078d672cd1e7a
Lahiru De Silva <lahirurashmitha00@gmail.com>
2024-05-17 11:45:12 +0530

    Update Jenkinsfile sonar analysis step and sonar_analysis.sh file

--- scripts/sonar_analysis.sh
+++ scripts/sonar_analysis.sh
@@ -1,31 +1,26 @@
 #!/bin/bash
 
 
-sonar_analysis() {
-    # Configuration variables
-    SONAR_IP=$1
-    SONAR_TOKEN="squ_9e4e156d70cd1caa4657005150d2466fd6f247a4"
-    SONAR_PROJECT_NAME="shopping-cart"
-    SONAR_BINARIES="."
-    SONAR_PROJECT_KEY="shopping-cart"
+# Check if SonarQube IP was provided as an argument
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <SonarQube IP>"
+    exit 1
+fi
 
-    # Maven sonarQube analysis
-    mvn sonar:sonar \
+# Change dir
+cd /home/GitOps-CI
+
+# Configuration variables
+SONAR_IP=$1
+SONAR_TOKEN="squ_9e4e156d70cd1caa4657005150d2466fd6f247a4"
+SONAR_PROJECT_NAME="shopping-cart"
+SONAR_BINARIES="."
+SONAR_PROJECT_KEY="shopping-cart"
+
+# Run Maven SonarQube analysis
+mvn sonar:sonar \
     -Dsonar.host.url="http://${SONAR_IP}:9000/" \
     -Dsonar.login="${SONAR_TOKEN}" \
     -Dsonar.projectName="${SONAR_PROJECT_NAME}" \
     -Dsonar.java.binaries="${SONAR_BINARIES}" \
     -Dsonar.projectKey="${SONAR_PROJECT_KEY}"
-}
-
-# Copy this file into /home/GitOps-CI
-copy_file() {
-    cp /home/scripts/sonar_analysis.sh /home/GitOps-CI
-}
-
-# Check first argument and call function accordingly
-if [ "$1" == "sonar_analysis" ]; then
-    sonar_analysis "$2"
-elif [ "$1" == "copy_file" ]; then
-    copy_file
-fi
